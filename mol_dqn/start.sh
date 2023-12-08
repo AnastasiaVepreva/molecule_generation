b1f24ea7d23cc2f7685268372bdcaa3e624edc50
Peter Zhilyaev <peterzhilyaev@gmail.com>
2023-12-08 08:03:16 +0000

    Add start.sh

--- mol_dqn/start.sh
+++ mol_dqn/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cd ./workspace/molecule_generation/mol_dqn
+git config --global user.email "peterzhilyaev@gmail.com"
+git config --global user.name "Peter Zhilyaev"
 poetry install
 export PYTHONPATH=/workspace/molecule_generation:/workspace/molecule_generation/mol_dqn:$PYTHONPATH
 poetry shell
