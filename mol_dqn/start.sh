c5dd7a7ed7842395a8740f6ec6af505adf5653e1
Peter Zhilyaev <peterzhilyaev@gmail.com>
2023-12-10 11:59:31 +0000

    Add to start.sh --no-root into poetry install

--- mol_dqn/start.sh
+++ mol_dqn/start.sh
@@ -3,7 +3,7 @@
 cd /workspace/molecule_generation/mol_dqn
 git config --global user.email "peterzhilyaev@gmail.com"
 git config --global user.name "Peter Zhilyaev"
-poetry install
+poetry install --no-root 
 export PYTHONPATH=/workspace/molecule_generation:/workspace/molecule_generation/mol_dqn:$PYTHONPATH
 cp optimizers.py $(poetry env info --path | head -1)/lib/python3.10/site-packages/tf_slim/layers/optimizers.py 
 poetry shell
