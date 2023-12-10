522b91ee2aa3a24cbfc687de522ec8411ef6c549
Peter Zhilyaev <peterzhilyaev@gmail.com>
2023-12-10 12:04:11 +0000

    Changing start.sh

--- mol_dqn/start.sh
+++ mol_dqn/start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+export PYTHONPATH=/workspace/molecule_generation:/workspace/molecule_generation/mol_dqn
 cd /workspace/molecule_generation/mol_dqn
 git config --global user.email "peterzhilyaev@gmail.com"
 git config --global user.name "Peter Zhilyaev"
 poetry install --no-root 
-export PYTHONPATH=/workspace/molecule_generation:/workspace/molecule_generation/mol_dqn:$PYTHONPATH
 cp optimizers.py $(poetry env info --path | head -1)/lib/python3.10/site-packages/tf_slim/layers/optimizers.py 
 poetry shell
